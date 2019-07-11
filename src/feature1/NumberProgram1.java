18d81eadee3d7cfd5f1d1d3984856fe51104e9b3
kaushal369@gmail.com <kaushal369@gmail.com>
2019-07-11 17:35:15 +0530

    Second commit on f1 branch

--- src/feature1/NumberProgram1.java
+++ src/feature1/NumberProgram1.java
@@ -3,8 +3,14 @@ package feature1;
 public class NumberProgram1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		// Addition of two number
+		
+		int a = 10;
+		int b= 20;
+		
+		int c = a+b;
+		System.out.println("The Addtition of two number is " + c);
 	}
 
 }
